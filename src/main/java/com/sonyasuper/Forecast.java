cca2a8fcad735a92e05ecefeac55027a49ac284c
whoscared <senatorova.ss@edu.spbstu.ru>
2022-05-19 01:27:12 +0300

    Налог на розовое

--- src/main/java/com/sonyasuper/Forecast.java
+++ src/main/java/com/sonyasuper/Forecast.java
@@ -4,6 +4,7 @@ public class Forecast {
 
     public static void main(String[] args) {
         String forecast = "День будет хороший, если проснетесь пораньше и выпьете чай вместо кофе";
+        String forecast1 = "Вам сегодня обязательно нужен розовый карандаш!(или фломастер)";
     }
     
 }
