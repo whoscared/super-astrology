652aa60d3a9e37eabfa4c90bfa41f917515fbb1a
Komissarov Gleb <gl.k@aol.com>
2022-05-19 00:46:12 +0300

    Initial commit

---
+++ src/main/java/com/sonyasuper/Forecast.java
@@ -0,0 +1,9 @@
+package com.sonyasuper;
+
+public class Forecast {
+
+    public static void main(String[] args) {
+        String forecast = "День будет хороший, если проснетесь пораньше и выпьете чай вместо кофе";
+    }
+    
+}
